1247b621c9eed303d002a6e2cf530a7f34678520
Honey Mathur <85191757+honeyherambmathur@users.noreply.github.com>
2021-06-08 09:43:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,13 @@
                 p = document.getElementById("p").value;
                 n = document.getElementById("n").value;
                 r = document.getElementById("r").value;
+                 y=n;
                 result = document.getElementById("result");
-                result.innerHTML = "The interest is " + (p*n*r/100);
+                if(p>0){
+                            result.innerHTML = "If you deposit "+ p +" amount, "+" at interest rate of "+ r +" you will recive back " + (p*n*r/100) + " , "+" after "+y+" years";
+                        }
+                else
+                {
+                    alert("Enter positive number");
+                }
             }
